05657b8b4d72231e6c9d518f5e33d64f67ea5d33
Sergey Bennett <Sergeybennett@gmail.com>
2017-06-25 01:37:53 +0300

    a
    a

---
+++ out/artifacts/ProjectUNA_1_war_exploded/WEB-INF/classes/static/js/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Illya on 6/24/17.
+ */
+
--- target/classes/insert.sql
+++ target/classes/insert.sql
@@ -1,4 +1,4 @@
 
-insert into user (email,enabled,pass_reset_date,nickname,password,birth,sex) VALUES ("user@email.com",true, "2017-06-19 01:01:01", "user", "$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC","1997-07-01" ,"male");
+insert into user (email,enabled,pass_reset_date,nickname,password,birth,sex) VALUES ("user@email.com",true, "2017-06-19 01:01:01", "user", "user","1997-07-01" ,"male");
 insert into authority (name) values ("ROLE_USER");
 insert into user_to_authority (user_id,authority_id) VALUES (1,1);
